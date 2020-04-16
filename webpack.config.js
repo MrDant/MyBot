c934dcf6c18a42afb088afdd4fb2c3fd8f7a8ede
MrDant <mael.dantuono@gmail.com>
2020-04-16 22:40:54 +0200

    fix webpack errors
    background.js
    contentScript.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ExtensionReloader = require("webpack-extension-reloader");
+const { VueLoaderPlugin } = require("vue-loader");
+const { version, description } = require("./package.json");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { VueLoaderPlugin } = require("vue-loader");
-const CopyPlugin = require("copy-webpack-plugin");
-const { version, description } = require("./package.json");
-const ExtensionReloader = require("webpack-extension-reloader");
-const webpack = require("webpack");
 
 const pathPolyfill = require.resolve("webextension-polyfill");
 const arrayCopyPlugin = [
@@ -21,93 +21,41 @@ const arrayCopyPlugin = [
         description
       });
 
-      if (pathPolyfill) {
-        Object.keys(jsonContent).map(function(key) {
-          if (jsonContent[key].scripts) {
-            jsonContent[key].scripts.unshift("browser-polyfill.js");
-          }
-        });
-      } else {
-        console.log(
-          "\x1b[33m-------------------------------------------------------\x1b[32m \n",
-          "exec :",
-          "\x1b[1m",
-          "npm install webextension-polyfill --save-dev;",
-          "\x1b[0m\x1b[32m",
-          "\n to apply a browser-polyfill extensions\n",
-          "\x1b[33m-------------------------------------------------------\x1b[0m \n\n"
-        );
+      if (config.mode === "development") {
+        jsonContent["content_security_policy"] =
+          "script-src 'self' 'unsafe-eval'; object-src 'self'";
       }
 
       return JSON.stringify(jsonContent, null, 2);
     }
   }
 ];
-if (pathPolyfill) {
-  arrayCopyPlugin.push({
-    from: pathPolyfill,
-    to: "browser-polyfill.js", // Where to copy the file in the destination folder
-    flatten: true // Don't keep the node_modules tree
-  });
-}
 const config = {
   mode: "development",
   context: __dirname + "/src",
   entry: {
     background: "./background.js",
+    contentScript: "./contentScript.js",
     "popup/popup": "./popup/popup.js",
     "options/options": "./options/options.js"
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-    chunkFilename: "[name].js"
+    path: __dirname + "/dist",
+    filename: "[name].js"
   },
   resolve: {
-    alias: {
-      vue$: "vue/dist/vue.esm.js"
-    },
-    extensions: ["*", ".js", ".vue", ".json"]
-  },
-  devtool: "inline-source-map",
-  plugins: [
-    new webpack.DefinePlugin({
-      global: "window"
-    }),
-    new CopyPlugin(arrayCopyPlugin),
-    new HtmlWebpackPlugin({
-      filename: "options/options.html",
-      template: "vue.html",
-      chunks: ["options/options"]
-    }),
-    new HtmlWebpackPlugin({
-      filename: "popup/popup.html",
-      template: "vue.html",
-      chunks: ["popup/popup"]
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    new VueLoaderPlugin()
-  ],
-  optimization: {
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all"
-        }
-      }
-    }
+    extensions: [".js", ".vue"]
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
         loaders: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
@@ -130,15 +78,36 @@ const config = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      global: "window"
+    }),
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new CopyPlugin(arrayCopyPlugin),
+    new HtmlWebpackPlugin({
+      filename: "options/options.html",
+      template: "vue.html",
+      chunks: ["options/options"]
+    }),
+    new HtmlWebpackPlugin({
+      filename: "popup/popup.html",
+      template: "vue.html",
+      chunks: ["popup/popup"]
+    })
+  ],
+  devtool: "inline-source-map"
 };
 
 if (process.argv.indexOf("--watch") > 0) {
-  config.plugins.unshift(
+  config.plugins = (config.plugins || []).concat([
     new ExtensionReloader({
       manifest: __dirname + "/src/manifest.json"
     })
-  );
+  ]);
 } else {
   config.plugins.unshift(new CleanWebpackPlugin());
 }
