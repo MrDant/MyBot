df69b353b0277e382019114b82dfc1b739a313c4
MrDant <mael.dantuono@gmail.com>
2020-04-19 11:44:27 +0200

    refactor filter
    update timelines
    add actions

--- src/asset/lib/filter.js
+++ src/asset/lib/filter.js
@@ -1,8 +1,14 @@
-export function timelines(timelines) {
-  let url = window.location.href;
+export function timelines(timelines, url) {
   timelines = Object.entries(timelines).filter(
-    entry => entry[0].match(url) && entry[1].activated
+    (entry) => entry[0].match(url) && entry[1].activated
   );
-  timelines = timelines.map(t => t[1]);
+  timelines = timelines.map((t) => t[1]);
+
   return timelines;
+}
+
+export function actions(timelines) {
+  return Object.keys(timelines)
+    .map((key) => timelines[key].actions)
+    .flat();
 }
